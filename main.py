acbb148aaa924d80950048d462e14522b9700849
Abdulaziz Musa <swiftv99@gmail.com>
2024-07-28 18:43:21 +0200

    Remove unnecessary lines

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
-from helpers import login_required, usd
+from helpers import login_required
 
 # Import the blueprints
 from app import add_password, change_password, delete_password, edit_password, login, logout, passwords_vault, register
@@ -20,9 +20,6 @@ app.register_blueprint(login.login_bp)
 app.register_blueprint(logout.logout_bp)
 app.register_blueprint(passwords_vault.passwords_vault_bp)
 app.register_blueprint(register.register_bp)
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
